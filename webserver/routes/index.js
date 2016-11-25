af449640585b7be326cc988577e520a075120b76
Gustavo <gustavomedeirosdasilva@gmail.com>
2016-11-25 14:24:38 -0200

    Pesquisa de pré-requisitos implementada.

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -9,13 +9,12 @@ var connection = mysql.createConnection
 	}
 );
 
-
-getSQLFormat = function(disciplinas, colunas, tabela)
+getSQLFormat = function(disciplinas, colunas, coluna_where, tabela)
 {
     var str = 'select ' + colunas + ' from ' + tabela + ' where ';
     for (i in disciplinas)
     {
-        str += '(codigo = \'' + disciplinas[i].codigo + '\')'
+        str += '(' + coluna_where + ' = \'' + disciplinas[i].codigo + '\')'
         if (i != disciplinas.length-1)
         {
             str += ' or ';
@@ -27,7 +26,7 @@ getSQLFormat = function(disciplinas, colunas, tabela)
 
 getCargaHoraria = function(req, res)
 {
-    var str = getSQLFormat(req.body.disciplinas, 'codigo, CH', 'disciplina');
+    var str = getSQLFormat(req.body.disciplinas, 'codigo, CH', 'codigo', 'disciplina');
 
     connection.query(str,
         function(err, rows, fields)
@@ -48,14 +47,27 @@ getCargaHoraria = function(req, res)
 
 // TODO: Tem que ver com os DBAs se será construída uma view para
 // Facilitar a consulta de pré-Requisitos!
-///// select * from preRequisito inner join disciplina on disciplina.id = preRequisito.disciplina where disciplina.codigo = 'BCD29008';
+////use bcd;
+////drop view if exists v_preRequisito;
+////create view v_preRequisito as
+////	select d1.codigo as disciplina, d2.codigo as preRequisito
+////	from preRequisito
+////	inner join disciplina as d1
+////	on preRequisito.disciplina = d1.id
+////	inner join disciplina as d2
+////	on preRequisito.preRequisito = d2.id;
+////
+////select * from v_preRequisito;
 getPreRequisito = function(req, res)
 {
-    var str = getSQLFormat(req.body.disciplinas, "", "");
+    var str = getSQLFormat(req.body.disciplinas, '*', 'disciplina', 'v_preRequisito');
+
+console.log('str: %s', str);
 
     connection.query(str,
         function(err, rows, fields)
         {
+console.log('row: %s', JSON.stringify(rows));
             if (!err)
             {
                 res.status(200);
@@ -94,4 +106,3 @@ exports.pre_requisito = function(req, res)
 {
     getPreRequisito(req, res);
 };
-
