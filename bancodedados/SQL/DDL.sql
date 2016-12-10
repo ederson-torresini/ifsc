0ebf28350c27a306fa759702c9750a96833f659a
bcd20162 <bcd20162@gmail.com>
2016-12-10 13:12:47 -0200

    UPDATE DDL.sql

--- bancodedados/SQL/DDL.sql
+++ bancodedados/SQL/DDL.sql
@@ -222,6 +222,7 @@ CREATE TABLE IF NOT EXISTS horario(
 	inicio time not null,
 	termino time not null,
 	turno varchar(16) not null,
+	semana varchar(2) not null,
 	dia smallint not null,
 	constraint fk_horario_disciplina_disciplina_id
     	foreign key(disciplina) references disciplina(iddisciplina),
