0e1d115127c4e5551fbb4956daec42b2211b4828
Ederson Torresini <boidacarapreta@gmail.com>
2018-11-06 10:34:39 -0200

    Aprender sobre: estruturas de repetição

--- juroComposto.py
+++ juroComposto.py
@@ -7,19 +7,19 @@ montante = total - entrada
 
 
 meses = []
+for mes in range(0, parcelas):
+    if (mes == 0):
+        meses.append(montante * juro)
+    else:
+        meses.append(meses[mes - 1] * juro)
+    print("Mês " + str(mes + 1) + ": " + str(round(meses[mes], 2)))
 
-if (len(meses) == 0):
-    meses.append(montante * juro)
-print("Mês 1: " + str(round(meses[0], 2)))
 
-meses.append(meses[0] * juro)
-print("Mês 2: " + str(round(meses[1], 2)))
 
-meses.append(meses[1] * juro)
-print("Mês 3: " + str(round(meses[2], 2)))
 
-meses.append(meses[2] * juro)
-print("Mês 4: " + str(round(meses[3], 2)))
 
-parcela = meses[3] / 4
+
+
+
+parcela = meses[parcelas - 1] / parcelas
 print("Parcela: " + str(round(parcela, 2)))
