1cddfc9ca837bee949be136ba5e36a7e8fa44011
Ederson Torresini <boidacarapreta@gmail.com>
2018-11-06 10:36:53 -0200

    Aprender sobre: importações

--- juroComposto.py
+++ juroComposto.py
@@ -1,19 +1,10 @@
-
+import biblioteca
 total = 800.0
 entrada = 0.0
 juro = 1.02
 parcelas = 4
 montante = total - entrada
 
-def juroComposto(quantidade, taxaDeJuros, qtdeParcelas):
-    meses = []
-    for mes in range(0, qtdeParcelas):
-        if (mes == 0):
-            meses.append(quantidade * taxaDeJuros)
-        else:
-            meses.append(meses[mes - 1] * taxaDeJuros)
-        print("Mês " + str(mes + 1) + ": " + str(round(meses[mes], 2)))
-    return meses[qtdeParcelas - 1]
 
 
 
@@ -21,5 +12,14 @@ def juroComposto(quantidade, taxaDeJuros, qtdeParcelas):
 
 
 
-parcela = juroComposto(montante, juro, parcelas) / parcelas
+
+
+
+
+
+
+
+
+
+parcela = biblioteca.juroComposto(montante, juro, parcelas) / parcelas
 print("Parcela: " + str(round(parcela, 2)))
