a0276f52895f39b1e46c0a8ef7cd88338233e005
Ederson Torresini <boidacarapreta@gmail.com>
2018-11-06 10:35:29 -0200

    Aprender sobre: funções

--- juroComposto.py
+++ juroComposto.py
@@ -5,21 +5,21 @@ juro = 1.02
 parcelas = 4
 montante = total - entrada
 
-
-meses = []
-for mes in range(0, parcelas):
-    if (mes == 0):
-        meses.append(montante * juro)
-    else:
-        meses.append(meses[mes - 1] * juro)
-    print("Mês " + str(mes + 1) + ": " + str(round(meses[mes], 2)))
-
-
-
+def juroComposto(quantidade, taxaDeJuros, qtdeParcelas):
+    meses = []
+    for mes in range(0, qtdeParcelas):
+        if (mes == 0):
+            meses.append(quantidade * taxaDeJuros)
+        else:
+            meses.append(meses[mes - 1] * taxaDeJuros)
+        print("Mês " + str(mes + 1) + ": " + str(round(meses[mes], 2)))
+    return meses[qtdeParcelas - 1]
 
 
 
 
 
-parcela = meses[parcelas - 1] / parcelas
+
+
+parcela = juroComposto(montante, juro, parcelas) / parcelas
 print("Parcela: " + str(round(parcela, 2)))
